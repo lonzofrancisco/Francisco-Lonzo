986bcefc879426ae6696a5b62385a76ad9646a39
lonzofrancisco <lonzofrancisco@gmail.com>
2022-10-05 13:20:53 -0300

    se agrego js para animar titulo

---
+++ js/index.js
@@ -0,0 +1,25 @@
+//maquina de escribir
+
+var text = document.getElementById("maquina");
+var str = text.innerHTML;
+
+text.innerHTML = str.charAt(0);
+
+const speed = 120;
+var i = 0;
+
+function maquina() {
+    if (i < str.length) {
+        text.innerHTML += str.charAt(i+1);
+        i++;
+        setTimeout(maquina, speed)
+
+    }else{
+        
+        text.innerHTML = "F";
+        i = 0
+        setTimeout(maquina, speed);
+    }
+}
+
+setTimeout(maquina,speed);
